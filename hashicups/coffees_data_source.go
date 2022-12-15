a4cd4b50c7b91051f6edca3eca3ae200b5dbf9a0
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-12-15 16:15:31 +0100

    Implement Data Source

--- hashicups/coffees_data_source.go
+++ hashicups/coffees_data_source.go
@@ -3,13 +3,18 @@ package hashicups
 import (
 	"context"
 
+	"github.com/hashicorp-demoapp/hashicups-client-go"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-type coffeesDataSource struct{}
+type coffeesDataSource struct {
+	client *hashicups.Client
+}
 
 var _ datasource.DataSource = (*coffeesDataSource)(nil)
+var _ datasource.DataSourceWithConfigure = (*coffeesDataSource)(nil)
 
 func NewCoffeesDataSource() datasource.DataSource {
 	return &coffeesDataSource{}
@@ -20,8 +25,89 @@ func (d *coffeesDataSource) Metadata(_ context.Context, req datasource.MetadataR
 }
 
 func (d *coffeesDataSource) Schema(_ context.Context, req datasource.SchemaRequest, res *datasource.SchemaResponse) {
-	res.Schema = schema.Schema{}
+	res.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"coffees": schema.ListNestedAttribute{
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.Int64Attribute{
+							Computed: true,
+						},
+						"name": schema.StringAttribute{
+							Computed: true,
+						},
+						"teaser": schema.StringAttribute{
+							Computed: true,
+						},
+						"description": schema.StringAttribute{
+							Computed: true,
+						},
+						"price": schema.Float64Attribute{
+							Computed: true,
+						},
+						"image": schema.StringAttribute{
+							Computed: true,
+						},
+						"ingredients": schema.ListNestedAttribute{
+							Computed: true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"id": schema.Int64Attribute{
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+				Computed: true,
+			},
+		},
+	}
 }
 
 func (d *coffeesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state coffeesDataSourceModel
+
+	coffees, err := d.client.GetCoffees()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read HashiCups Coffees",
+			err.Error(),
+		)
+		return
+	}
+
+	for _, coffee := range coffees {
+		coffeeState := coffeesModel{
+			ID:          types.Int64Value(int64(coffee.ID)),
+			Name:        types.StringValue(coffee.Name),
+			Teaser:      types.StringValue(coffee.Teaser),
+			Description: types.StringValue(coffee.Description),
+			Price:       types.Float64Value(coffee.Price),
+			Image:       types.StringValue(coffee.Image),
+		}
+		for _, ingredient := range coffee.Ingredient {
+			coffeeState.Ingredients = append(coffeeState.Ingredients, coffeesIngredientsModel{
+				ID: types.Int64Value(int64(ingredient.ID)),
+			})
+		}
+		state.Coffees = append(state.Coffees, coffeeState)
+	}
+
+	// Set state
+	diags := resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *coffeesDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	d.client = req.ProviderData.(*hashicups.Client)
 }
--- hashicups/model.go
+++ hashicups/model.go
@@ -9,3 +9,24 @@ type hashicupsProviderModel struct {
 	Username types.String `tfsdk:"username"`
 	Password types.String `tfsdk:"password"`
 }
+
+// coffeesDataSourceModel maps the data source schema data.
+type coffeesDataSourceModel struct {
+	Coffees []coffeesModel `tfsdk:"coffees"`
+}
+
+// coffeesModel maps coffees schema data.
+type coffeesModel struct {
+	ID          types.Int64               `tfsdk:"id"`
+	Name        types.String              `tfsdk:"name"`
+	Teaser      types.String              `tfsdk:"teaser"`
+	Description types.String              `tfsdk:"description"`
+	Price       types.Float64             `tfsdk:"price"`
+	Image       types.String              `tfsdk:"image"`
+	Ingredients []coffeesIngredientsModel `tfsdk:"ingredients"`
+}
+
+// coffeesIngredientsModel maps coffee ingredients data
+type coffeesIngredientsModel struct {
+	ID types.Int64 `tfsdk:"id"`
+}
